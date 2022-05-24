6535bc8f906b5f3f0add98d8db3a0982b26b81a1
grasmano <76538570+grasmano@users.noreply.github.com>
2022-05-24 18:59:42 +0300

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -16,25 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
-Route::middleware('api')->group(function () {
-    Route::resource('campaigns', CampaignController::class);
-});
-
-Route::middleware('api')->group(function () {
-    Route::resource('creatives', CreativeController::class);
-});
-
-//Route::resource('campaign',App\Http\Controllers\CampaignController::class)->only(['index','store','show','update','destroy']);
-//
-//
-//Route::get('posts', 'PostController@index');
-//Route::group(['prefix' => 'post'], function () {
-//    Route::post('add', 'PostController@add');
-//    Route::get('edit/{id}', 'PostController@edit');
-//    Route::post('update/{id}', 'PostController@update');
-//    Route::delete('delete/{id}', 'PostController@delete');
-//});
+Route::resource('campaigns',App\Http\Controllers\CampaignController::class)->only(['index','store']);
